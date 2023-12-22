da2667cc4d964c713aa72f736bec9a99ed9982d9
Omkar21-1 <omkarkshirsagar2005@gmail.com>
2023-12-22 17:51:41 +0530

    pravin_ksh.c --> sum of given no. + reverse no.

---
+++ pravin_ksh.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+int main()
+{
+	int n;
+	int temp_n;
+	int a;
+	int rev=0;
+	
+	printf("Enter Number : ");
+	scanf("%d",&n);
+	
+	temp_n = n;
+	
+	while(temp_n>0)
+	{
+		a = temp_n%10;
+		rev = (rev*10)+a;
+		temp_n = temp_n/10;
+	}
+	
+	printf("Sum of Given no. + Reverse no. : %d\n",n+rev);
+
+	return 0;
+}
