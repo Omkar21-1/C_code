e62ae3e8e8ff827611a512a7ac6dccfacde46227
Omkar21-1 <omkarkshirsagar2005@gmail.com>
2024-02-06 23:31:22 +0530

    test

--- C_codes/clg/clg_program.c
+++ C_codes/clg/clg_program.c
@@ -1,37 +1,9 @@
 #include<stdio.h>
-void main()
+
+void display();
+
+void display()
 {
-    int matrix1[3][3],matrix2[3][3],matrixresult[3][3];
-    printf("Enter the value of matrix\n");
-    for(int i=0;i<3;i++)
-    {
-        for(int j=0;j<3;j++)
-        {
-        printf("Enter value %d %d  ",i,j);
-        scanf("%d",&matrix1[i][j]);
-        }printf("\n");
-    }
-    printf("Enter the values of matrix 2\n");
-    for(int i=0;i<3;i++)
-    {
-        for(int j=0;j<3;j++)
-        {
-        printf("Enter value %d %d  ",i,j);
-        scanf("%d",&matrix2[i][j]);
-        }printf("\n");
-    }
-    for(int i=0;i<3;i++)
-    {
-        for(int j=0;j<3;j++)
-        {
-            matrixresult[i][j] = matrix1[i][j] + matrix2[i][j];
-        }
-    }
-     for(int i=0;i<3;i++)
-    {
-        for(int j=0;j<3;j++)
-        {
-        printf("%3d",matrixresult[i][j]);
-    	}printf("\n");
-    }
+	printf("Hello World\n");
+	
 }
